ce214612a6f9cdde17d838064735a274fcb69f9c
alfret <alfredo163217@gmail.com>
2023-04-22 18:55:31 -0600

    avances de la tarea

---
+++ js/Tarea1.js
@@ -0,0 +1,79 @@
+/*
+Ejercicio 2
+let nombre = prompt("Escriba el nombre de la pelicula")
+alert("El director de esta pelicula es SAMM MENDEZ")
+
+document.write(nombre)
+document.write("<br>")
+*/
+
+
+let Peliculas = [
+
+    {
+        pelicula: "1917",
+        isFree: false,
+    },
+
+    {
+        pelicula: "el instante mas obscuro",
+        isFree: true,
+    },
+
+    {
+        pelicula: "ROMA",
+        isFree: true,
+    },
+
+   
+]
+if(Peliculas[1]){
+    console.log(isFree)      
+}
+
+
+/*
+Ejercicio 5
+let mes = 10
+
+switch (mes){
+    case 1:
+        alert("Enero")
+    break;
+    case 2:
+        alert("Febrero")
+    break;
+    case 3:
+        alert("Marzo")
+    break;
+    case 4:
+        alert("Abril")
+    break;
+    case 5:
+        alert("Mayo")
+    break;
+    case 6:
+        alert("Junio")
+    break;
+    case 7:
+        alert("Julio")
+    break;
+    case 8:
+        alert("Agosto")
+    break;
+    case 9:
+        alert("Septiembre")
+    break;
+    case 10:
+        alert("Octubre")
+    break;
+    case 11:
+        alert("Noviembre")
+    break;
+    case 12:
+        alert("Diciembre")
+    break;
+    default:
+        alert("Dato incorrecto")
+    break;
+}*/
